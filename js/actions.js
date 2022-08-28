e854729636348396042339b8051652dbbde1f9b4
dadlian <sveninem@gmail.com>
2022-08-28 17:27:57 +0100

    Made site mobile responsive. Finished Sven's story

--- js/actions.js
+++ js/actions.js
@@ -1,9 +1,23 @@
 function openMenu(){
-  document.getElementById("main_menu").classList.add("active")
-  document.getElementsByClassName("page")[0].style.overflowY = 'hidden'
+  setTimeout(() => {
+    document.getElementById("main_menu").classList.add("active")
+    document.getElementsByClassName("page")[0].style.overflowY = 'hidden'
+  })
 }
 
 function closeMenu(){
   document.getElementById("main_menu").classList.remove("active")
   document.getElementsByClassName("page")[0].style.overflowY = 'scroll'
 }
+
+function globalClick(event){
+  if(
+      event.target.id !== "main_menu" &&
+      event.target.parentElement.id != "main_menu" &&
+      document.getElementById("main_menu").classList[1] == 'active'
+  ){
+    closeMenu()
+  }
+}
+
+document.body.onclick = globalClick
